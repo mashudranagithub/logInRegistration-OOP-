39c9d583935c6e36939c6e33e39279087d16dd20
mashud <mashudranabitm@gmail.com>
2017-07-31 00:54:38 +0600

    Front end word done for login & registration form

---
+++ login.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Form</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form action="" method="post">
+
+
+                <label for="username">Username</label>
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" id="username" placeholder="Enter your username here">
+                </div>
+
+
+                <label for="password">Password</label>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" id="password" placeholder="Enter your password here">
+                </div>
+
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
+</div>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration Form</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form action="" method="post">
+
+
+                <label for="fname">First Name</label>
+                <div class="form-group">
+                    <input class="form-control" type="text" name="fname" id="fname" placeholder="Enter your first name here">
+                </div>
+
+                <label for="lname">Last Name</label>
+                <div class="form-group">
+                    <input class="form-control" type="text" name="lname" id="lname" placeholder="Enter your last name here">
+                </div>
+
+                <label for="email">Email</label>
+                <div class="form-group">
+                    <input class="form-control" type="email" name="email" id="email" placeholder="Enter your email name here">
+                </div>
+
+                <label for="username">Username</label>
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" id="username" placeholder="Enter your username here">
+                </div>
+
+                <label for="password">Password</label>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" id="password" placeholder="Enter your password here">
+                </div>
+
+                <input type="submit" value="Register">
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
