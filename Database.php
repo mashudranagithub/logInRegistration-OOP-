3150005d35eadb1c4bb163b575dd3e425cf34406
mashud <mashudranabitm@gmail.com>
2017-07-31 08:21:41 +0600

    Database class created

---
+++ Database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Database{
+    private $dbhost = "localhost";
+    private $dbname = "login-register";
+    private $dbuser = "root";
+    private $dbpass = "";
+    public $pdo;
+
+    public function __construct()
+    {
+        if(isset($this->pdo)){
+            try{
+                $conn = new PDO("mysql:host=".$this->dbhost.";dbname=".$this->dbname,$this->dbuser,$this->dbpass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->pdo = $conn;
+            }catch (PDOException $ex){
+                die("Database connection failed".$ex->getMessage());
+            }
+        }
+    }
+}
